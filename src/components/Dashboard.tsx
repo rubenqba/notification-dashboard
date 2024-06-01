656b6f293af993225764618c7a3c84351bdd8420
Ruben Bresler <ruben.bresler@ext.cemex.com>
2024-06-01 21:28:53 +0000

    Update message

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -2,7 +2,7 @@ import { Card, CardContent, CardHeader } from '@mui/material';
 
 export const Dashboard = () => (
     <Card>
-        <CardHeader title="Welcome to the administration" />
+        <CardHeader title="Processed notifications" />
         <CardContent>Lorem ipsum sic dolor amet...</CardContent>
     </Card>
 );
