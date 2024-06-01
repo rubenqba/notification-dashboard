ba0f9a3e9dd4e1adba9ebae5e13da223779e4716
Ruben Bresler <111847+rubenqba@users.noreply.github.com>
2024-06-01 21:49:16 +0000

    Testing sign key in devcontainer

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -3,6 +3,6 @@ import { Card, CardContent, CardHeader } from '@mui/material';
 export const Dashboard = () => (
     <Card>
         <CardHeader title="Processed notifications" />
-        <CardContent>Lorem ipsum sic dolor amet...</CardContent>
+        <CardContent>Put some charts here...</CardContent>
     </Card>
 );
